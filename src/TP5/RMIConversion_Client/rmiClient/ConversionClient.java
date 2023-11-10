077473e4be25418bb24434915b729d490ee9f7b7
ahmed <ahmedadam.boussarsar@gmail.com>
2023-11-11 00:28:17 +0100

    Commit 5-1

--- src/TP5/RMIConversion_Client/rmiClient/ConversionClient.java
+++ src/TP5/RMIConversion_Client/rmiClient/ConversionClient.java
@@ -10,8 +10,8 @@ public class ConversionClient{
             // Recherche de l'objet distant par son URL RMI
             IConversion stub = (IConversion)Naming.lookup("rmi://localhost:1099/OD");
             // Appel de méthodes distantes et impression des résultats
-            System.out.println("Conversion result for 7000.0: " + stub.convertirMontant(420.0));
-            System.out.println("Conversion result for 60.0: " + stub.convertirMontant(64.0));
+            System.out.println("Conversion result for 420.0: " + stub.convertirMontant(420.0));
+            System.out.println("Conversion result for 64.0: " + stub.convertirMontant(64.0));
         }
         catch (Exception e)
         {e.PrintStackTrace();}
