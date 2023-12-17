1306315c9bfbd8a5bc843ae179514d31975160de
ahmed <ahmedadam.boussarsar@gmail.com>
2023-12-17 17:50:47 +0100

    Commit TP7

--- src/main/java/org/example/Consumer.java
+++ src/main/java/org/example/Consumer.java
@@ -7,16 +7,17 @@ import javax.jms.*;
 public class Consumer {
     public static void main(String[] args) {
         try {
-            //etablissement d'une connexion ou broker ActiveMQ
+            //etablissement d'une connexion au broker ActiveMQ et lancement
             ConnectionFactory conx = new ActiveMQConnectionFactory("tcp://localhost:61616");
             Connection connection = conx.createConnection();
             connection.start();
-
-            //creation d'un session
+            //creation d'une session (representant le context)
             Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            //Definir la destination du message
             Destination destination = session.createTopic("MyTopic.topic");
-
+            //Creation du MessageConsumer (Lier entre la session et la destination)
             MessageConsumer consumer = session.createConsumer(destination);
+            //Creation d'un JMS Listener pour traiter les messages dès leur disponibilité
             consumer.setMessageListener(new MessageListener() {
                 @Override
                 public void onMessage(Message message) {
--- src/main/java/org/example/Producer.java
+++ src/main/java/org/example/Producer.java
@@ -3,27 +3,29 @@ package org.example;
 import javax.jms.*;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-
 public class Producer {
     public static void main(String[] args) {
         try {
-            //etablissement d'une connexion ou broker ActiveMQ
+            //Connexion ou broker ActiveMQ
             ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+            //Creation d'une connection et lancement
             Connection connection = connectionFactory.createConnection();
             connection.start();
-
-            //creation d'un session
+            //creation d'une session
             Session session = ((javax.jms.Connection) connection).createSession(true, Session.AUTO_ACKNOWLEDGE);
+            //Definir la destination du msg
             Destination destination = session.createTopic("myTopic.topic");
+            //Creation du MessageProducer
             MessageProducer producer = session.createProducer(destination);
             producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+            //Creation du smg
             TextMessage textMessage = session.createTextMessage();
             textMessage.setText("Hello world!");
             producer.send(textMessage);
-
-            //commit the transaction
+            //commit de la transaction
             session.commit();
             System.out.println("Envoi du message...");
+            //Fermeture
             session.close();
             connection.close();
         } catch (Exception e) {
